7ffa6b2b7a81aab83bf7d1f38980845f06485bc2
Joel Robertson <robertson0318@neo.rr.com>
2023-12-15 21:05:02 -0500

    Initialize mod

---
+++ src/main/java/goliath/loginrewards/LoginRewards.java
@@ -0,0 +1,16 @@
+package goliath.loginrewards;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoginRewards implements ModInitializer {
+	public static final String MOD_ID = "loginrewards";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
